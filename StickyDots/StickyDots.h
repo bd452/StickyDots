28aaf9fe602c93e912c47af62361c30c4f2cf81a
Bryce Dougherty <bryce.dougherty@gmail.com>
2019-10-19 11:45:42 -0400

    Initial Commit

---
+++ StickyDots/StickyDots.h
@@ -0,0 +1,19 @@
+//
+//  StickyDots.h
+//  StickyDots
+//
+//  Created by Bryce Dougherty on 10/19/19.
+//  Copyright © 2019 The Farm. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for StickyDots.
+FOUNDATION_EXPORT double StickyDotsVersionNumber;
+
+//! Project version string for StickyDots.
+FOUNDATION_EXPORT const unsigned char StickyDotsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <StickyDots/PublicHeader.h>
+
+
